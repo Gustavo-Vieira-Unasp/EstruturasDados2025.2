5756b011ad768d41293a0b5f8101fd74a273d6fe
Gustavo Vieira <fonseca_vieira@educadventista.org>
2025-11-18 22:27:59 -0300

    mercado

--- desafio-18112025/src/models/superMrecado.py
+++ desafio-18112025/src/models/superMrecado.py
@@ -1,4 +1,12 @@
 from src.models.produtos import Produto
+import datetime
 
 def gerar_produtos(produto : Produto):
-    batata = Produto("Batata", )
+    categoria = ["Alimentos", "Limpeza", "Higiene", "Utensilhos"]
+    batata = Produto("Batata", 0, categoria[0], "alta", "Pendente", datetime.datetime.now())
+    smartphone = Produto("Smartphone XZ-500", 0, categoria[1], "alta", "Pendente", datetime.datetime.now())
+    camiseta = Produto("Camiseta", 0, categoria[2], "media", "Pendente", datetime.datetime.now())
+    livro_misterio = Produto("Livro", 0, categoria[3], "baixa", "Pendente", datetime.datetime.now())
+    pasta_dente = Produto("Pasta de Dente", 0, categoria[4], "alta", "Pendente", datetime.datetime.now())
+    fone_ouvido = Produto("Fone de Ouvido", 0, categoria[1], "media", "Pendente", datetime.datetime.now())
+    pao_integral = Produto("Pão", 0, categoria[0], "media", "Pendente", datetime.datetime.now())
